d2abb1f09d60577eda1209a3bfaccc31a3202686
Jack <jackhasakeyboard@gmail.com>
2021-12-07 10:47:34 -0700

    Something

--- shad.fs
+++ shad.fs
@@ -2,6 +2,17 @@
 
 in vec3 _pos;
 
+vec3 sun = vec3(1.0, 1.0, 1.0);
+
+vec3[2] col = vec3[2](
+	vec3(38, 38, 38),
+	vec3(255, 155, 71)
+);
+
 void main() {
-	gl_FragColor = vec4(_pos, 1.0);
+	vec3 normFace = normalize(cross(dFdx(_pos), dFdy(_pos)));
+
+	float ins = max(dot(normFace, normalize(sun)), 0.0);
+
+	gl_FragColor = vec4(ins * (col[int(true)] / 255.0), 1.0);
 }
