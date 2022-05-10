43e77897ef81588faf708de6a8ca98cabf9b75a7
Jack <jackhasakeyboard@gmail.com>
2022-05-10 14:07:31 -0600

    Implemented directional lighting

--- res/shad/tex.fs
+++ res/shad/tex.fs
@@ -5,6 +5,12 @@ in vec2 _st;
 
 uniform sampler2D tex;
 
+vec3 sun = vec3(1.0, 1.0, 1.0);
+
 void main() {
-	gl_FragColor = texture(tex, _st);
+	vec3 normFace = normalize(cross(dFdx(_pos), dFdy(_pos)));
+
+	float ins = max(dot(normFace, normalize(sun)), 0.0);
+
+	gl_FragColor = vec4(ins * texture(tex, _st).rgb, 1.0);
 }
