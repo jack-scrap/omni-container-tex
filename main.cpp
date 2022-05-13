17fdc6bb15850a3cca4f4cda72a4174b2a59fcd8
Jack <jackhasakeyboard@gmail.com>
2022-05-13 00:03:47 -0600

    Disabled scaling via mouse wheel

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@
 #include "disp.h"
 #include "prog.h"
 #include "util.h"
-#include "stb_image.h"
 #include "prop.h"
 #include "col.h"
 
@@ -19,7 +18,7 @@ extern Disp disp("asdf", 800, 600);
 int main() {
 	Prop prop("cont", "0");
 
-	glm::vec3 scale = glm::vec3(0.001, 0.001, 0.001);
+	glm::vec3 scale = glm::vec3(1.0, 1.0, 1.0);
 
 	bool pan = false;
 
@@ -36,24 +35,6 @@ int main() {
 				if (e.key.keysym.sym == SDLK_F1) {
 					prop._showWire = !prop._showWire;
 				}
-			}
-
-			if (e.type == SDL_MOUSEWHEEL) {
-				if (e.wheel.y > 0) {
-					if (glm::all(glm::lessThan(scale, glm::vec3(0.01, 0.01, 0.01)))) {
-						scale += glm::vec3(0.001, 0.001, 0.001);
-					}
-				}
-
-				if (e.wheel.y < 0) {
-					if (glm::all(glm::greaterThan(scale, glm::vec3(0.01, 0.01, 0.01)))) {
-						scale -= glm::vec3(0.001, 0.001, 0.001);
-					}
-				}
-
-				prop._model = glm::mat4(1.0);
-				prop._model = glm::rotate(prop._model, (GLfloat) (r * (M_PI / 100.0)), glm::vec3(0, 1, 0));
-				prop._model = glm::scale(prop._model, scale);
 			}
 
 			if (e.type == SDL_MOUSEBUTTONDOWN) {
